0fae1e3b20b779830ae5009dd45bb842d80965a3
RafaellaB <rafaellabmoura@gmail.com>
2023-07-12 00:31:43 -0300

    upgrade análise exploratória inicial

--- app.py
+++ app.py
@@ -1,25 +1,61 @@
-# importando as bibliotecas necessárias
 import pandas as pd
 import seaborn as sns
+import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 import altair as alt
 import plotly.express as px
 import plotly.graph_objects as go
-import streamlit as st
 from datetime import datetime
-import plotly.express as px
 
-@st.cache_resource
-def load_data(file_path):
-    data = pd.read_csv(file_path)
-    return data
+# Configuração para remover o aviso "PyplotGlobalUseWarning"
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
-file_path = "https://github.com/Taciana3090/PSI3/raw/Taciana3090/master/data/Life%20Expectancy%20Data.csv"
-df = load_data(file_path)
+# Definindo as configurações da página
+st.set_page_config(
+    page_title="Projeto III - Expectativa de Vida",
+    page_icon='📊',
+    layout="wide",
+    initial_sidebar_state="auto"
+)
 
-# Centraliza o título utilizando HTML
-st.write("<h1 style='text-align: center;'>Dashboard Life Expectancy</h1>", unsafe_allow_html=True)
-st.write("""
-Este dashboard tem como objetivo apresentar uma análise dos dados sobre o dataset Life Expectancy. 
-""")
+# URL do arquivo
+file_url = "https://github.com/Taciana3090/PSI3/raw/Taciana3090/master/data/Life%20Expectancy%20Data.csv"
+
+# Carregar o conjunto de dados
+df = pd.read_csv(file_url)
+
+# Título e descrição
+st.write("# Dashboard Expectativa de Vida")
+st.markdown("Este dashboard tem como objetivo apresentar uma análise interativa da expectativa de vida em diferentes países.")
+st.markdown("O conjunto de dados abrange o período de 2000 a 2015 e contém informações de 193 países.")
+
+# Páginas
+def data_page():
+    st.subheader("Visão Geral dos Dados")
+    
+    # Exibir a quantidade de linhas e colunas
+    st.write(f"Total de linhas: {df.shape[0]}")
+    st.write(f"Total de colunas: {df.shape[1]}")
+    
+    # Exibir as primeiras linhas do DataFrame
+    st.subheader("Amostra dos Dados")
+    st.write(df.head())
+    
+    # Contagem dos valores ausentes
+    st.subheader("Contagem de Valores Ausentes")
+    st.write(df.isnull().sum())
+    
+    # Estatísticas descritivas das colunas numéricas
+    st.subheader("Estatísticas Descritivas (Colunas Numéricas)")
+    st.write(df.describe())
+
+# Sidebar com botões
+pages = {
+    "Análise Exploratória Inicial": data_page
+   
+}
+
+# Renderizar a página selecionada
+selection = st.sidebar.radio("Selecione uma Página", list(pages.keys()))
+pages[selection]()
