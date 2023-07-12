14c12c985944bda1d39730919805d898aa9ceef1
Taciana Vasconcelos <83678468+Taciana3090@users.noreply.github.com>
2023-07-12 00:39:49 -0300

    Update filtros e gráficos iniciais

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ import plotly.express as px
 import plotly.graph_objects as go
 from datetime import datetime
 
-# Configuração para remover o aviso "PyplotGlobalUseWarning"
+#  remove o aviso "PyplotGlobalUseWarning"
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
-# Definindo as configurações da página
+# definindo as configurações da página
 st.set_page_config(
     page_title="Projeto III - Expectativa de Vida",
     page_icon='📊',
@@ -50,9 +50,51 @@ def data_page():
     st.subheader("Estatísticas Descritivas (Colunas Numéricas)")
     st.write(df.describe())
 
+def filters_page():
+    st.subheader("Filtros")
+    selected_country = st.sidebar.selectbox('Selecione um país', df['Country'].unique())
+    selected_year = st.sidebar.selectbox('Selecione um ano', df['Year'].unique())
+    selected_status = st.sidebar.selectbox('Selecione o status', df['Status'].unique())
+
+    # aplicando o filtro de country, ano e status
+    filtered_df = df[(df['Country'] == selected_country) & (df['Year'] == selected_year) & (df['Status'] == selected_status)]
+
+    # exibindo resultado
+    st.subheader("Resultado")
+    if filtered_df.empty:
+        st.warning("Nenhum resultado encontrado com os filtros selecionados.")
+    else:
+        st.dataframe(filtered_df)
+
+def charts_page():
+    
+    # selecionando o país, status e variável
+    selected_country = st.sidebar.selectbox('Selecione um país', df['Country'].unique())
+    selected_status = st.sidebar.selectbox('Selecione o status', df['Status'].unique())
+    selected_variable = st.sidebar.selectbox('Selecione uma variável', df.select_dtypes(include='number').columns)
+    
+    # filtrando o com base nos filtros selecionados
+    filtered_df = df[(df['Country'] == selected_country) & (df['Status'] == selected_status)]
+    if filtered_df.empty:
+        st.warning("Nenhum resultado encontrado com os filtros selecionados.")
+    else:
+        # gráfico de Linhas para a variável selecionada ao longo do tempo
+        st.subheader(f"{selected_variable} ao longo do tempo")
+        fig = go.Figure()
+        fig.add_trace(go.Scatter(x=filtered_df['Year'], y=filtered_df[selected_variable], mode='lines+markers', name=selected_variable))
+        z = np.polyfit(filtered_df['Year'], filtered_df[selected_variable], 1)
+        p = np.poly1d(z)
+        fig.add_trace(go.Scatter(x=filtered_df['Year'], y=p(filtered_df['Year']), mode='lines', name='Tendência'))
+        fig.update_layout(showlegend=True)
+        st.plotly_chart(fig)
+
+    
+
 # Sidebar com botões
 pages = {
-    "Análise Exploratória Inicial": data_page
+    "Análise Exploratória Inicial": data_page,
+    "Filtros": filters_page,
+    "Gráficos": charts_page
    
 }
 
