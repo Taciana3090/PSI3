a863225e1cee978d9644557b77bc032a472166fd
RafaellaB <rafaellabmoura@gmail.com>
2023-07-12 00:43:54 -0300

    Update informações iniciais

--- app.py
+++ app.py
@@ -87,14 +87,55 @@ def charts_page():
         fig.add_trace(go.Scatter(x=filtered_df['Year'], y=p(filtered_df['Year']), mode='lines', name='Tendência'))
         fig.update_layout(showlegend=True)
         st.plotly_chart(fig)
+        
+
+ # Função para a página de Informações
+def info_page():
+    st.subheader("Informações")
+     
+
+    # Adicionar mais informações
+    
+    st.write("<div style='border: 3px solid black; padding: 10px; background-color: #2F4F4F; font-weight: bold;'>Especificações sobre os atributos</div>", unsafe_allow_html=True)
+
+    st.write("<div style='border: 3px solid black; padding: 10px;'> </div>", unsafe_allow_html=True)
+   
+   
+    st.write("<div style='border: 1px solid black; padding: 10px;'>country: O nome do país.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>year: O ano correspondente aos dados.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>status: O status de desenvolvimento do país (por exemplo, desenvolvido ou em desenvolvimento).</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>life_expectancy: A expectativa de vida média em anos.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>adult_mortality: A taxa de mortalidade de adultos por 1.000 pessoas.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>infant_deaths: O número de mortes de crianças com menos de 1 ano.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>alcohol: O consumo de álcool por litro de álcool puro.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>percentage_expenditure: A despesa com saúde como uma porcentagem do Produto Interno Bruto (PIB) per capita.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>hepatitis_b: A cobertura vacinal contra hepatite B em crianças de 1 ano.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>measles: O número de casos de sarampo reportados.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>bmi: O índice de massa corporal médio da população.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>under-five_deaths: O número de mortes de crianças menores de 5 anos.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>polio: A cobertura vacinal contra poliomielite em crianças de 1 ano.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>total_expenditure: A despesa total com saúde como uma porcentagem do PIB.</div>", unsafe_allow_html=True)    
+    st.write("<div style='border: 1px solid black; padding: 10px;'>diphtheria: A cobertura vacinal contra difteria em crianças de 1 ano.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>hiv/aids: A prevalência de HIV/AIDS por 1.000 pessoas.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>gdp: O Produto Interno Bruto per capita.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>population: A população total.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>thinness_1-19_years: A prevalência de desnutrição em pessoas de 1 a 19 anos.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>thinness_1-19_years: A prevalência de desnutrição em pessoas de 1 a 19 anos.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>thinness_5-9_years: A prevalência de desnutrição em crianças de 5 a 9 anos.</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>income_composition_of_resources: O índice de composição de recursos (indicador do desenvolvimento humano).</div>", unsafe_allow_html=True)
+    st.write("<div style='border: 1px solid black; padding: 10px;'>schooling: A média de anos de escolaridade.</div>", unsafe_allow_html=True)
 
     
+
+    # Adicionar um link
+    st.markdown("[Saiba mais sobre a fonte dos dados](https://www.kaggle.com/datasets/kumarajarshi/life-expectancy-who)")   
 
 # Sidebar com botões
 pages = {
     "Análise Exploratória Inicial": data_page,
     "Filtros": filters_page,
-    "Gráficos": charts_page
+    "Gráficos": charts_page,
+    "Informações": info_page
    
 }
 
